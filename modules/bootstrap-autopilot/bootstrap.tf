7e29999fff5392d431bb46485d73d386112121d9
William Polinchak <polinchw@gmail.com>
2022-10-19 08:48:03 -0400

    Setting dependency on cluster_name.

--- modules/bootstrap-autopilot/bootstrap.tf
+++ modules/bootstrap-autopilot/bootstrap.tf
@@ -5,6 +5,7 @@ resource "null_resource" "bootstrap_autopilot" {
       KUBECONFIG = var.kubeconfig_file
       GIT_TOKEN  = var.git_token
       GIT_REPO   = var.git_repo
+      CLUSTER_NAME = var.cluster_name
      }
   }
   
