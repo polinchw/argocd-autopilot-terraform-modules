690fd83a17081cf606ff4da75b3d404dbb3b55a5
William Polinchak <polinchw@gmail.com>
2022-10-21 09:35:02 -0400

    Fixed path.

--- modules/install-autopilot/autopilot.tf
+++ modules/install-autopilot/autopilot.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "install_autopilot" {
   provisioner "local-exec" {
-    command     = "./modules/autopilot/install_autopilot.sh"
+    command     = "./modules/install-autopilot/install_autopilot.sh"
     interpreter = ["/bin/bash", "-c"]
   }
 }
