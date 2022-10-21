0049b7c187b001520f3cf51bc8f90fa164d270eb
William Polinchak <polinchw@gmail.com>
2022-10-21 10:04:50 -0400

    Correcting path.

--- modules/install-autopilot/autopilot.tf
+++ modules/install-autopilot/autopilot.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "install_autopilot" {
   provisioner "local-exec" {
-    command     = "./modules/install-autopilot/install_autopilot.sh"
+    command     = ".terraform/modules/install_autopilot/modules/install-autopilot/install_autopilot.sh"
     interpreter = ["/bin/bash", "-c"]
   }
 }
